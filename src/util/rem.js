f73b82ce146e88a1767ae7e5206b137780c89a14
pengle <1228561702@qq.com>
2023-01-10 09:35:05 +0800

    rem 375

--- src/util/rem.js
+++ src/util/rem.js
@@ -25,7 +25,7 @@ window.onresize = function () {
 //     var recalc = function () {
 //         var clientWidth = docEl.clientWidth;
 //         if (!clientWidth) return;
-//         docEl.style.fontSize = 16 * (clientWidth / 320) + 'px';
+//         docEl.style.fontSize = 16 * (clientWidth / 375) + 'px';
 //     };
 //     if (!doc.addEventListener) return;
 //     win.addEventListener(resizeEvt, recalc, false);
